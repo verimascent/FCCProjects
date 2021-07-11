29684e5b94c9e7eae00d8b706b53b8bfad7a3ad9
verimascent <verimascent@gmail.com>
2021-07-11 23:49:57 +0800

    RandomQuote - commit 1

---
+++ FrontEndDevelopmentLibraries/RandomQuoteMachine/src/API.ts
@@ -0,0 +1,10 @@
+export type Quotes = {
+    _id: string;
+    tags: string[];
+    content: string;
+    author: string;
+    authorSlug: string;
+    length: number;
+    dateAdded: string;
+    dateModified: string;
+}
